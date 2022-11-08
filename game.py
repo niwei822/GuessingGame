ab680bdac8c8a0e148800ac0eee75cb3015c2fa0
niwei822 <cecily.li.822@gmail.com>
2022-11-07 21:02:02 -0800

    add Limit the Number of Guesses a User Gets and a Range Option

--- game.py
+++ game.py
@@ -6,35 +6,42 @@ print(f"{name}, I'm thinking of a number between 1 and 100.")
 print("Try to guess my number.")
 moreRounds = "y"
 scores = []
+maxCount = 5
 def guessing():
-    low = 1
-    high = 100
-    fix_number = random.randint(low, high)
-    print(fix_number)
+    low = int(input("Choose your start number: "))
+    high = int(input("Choose your end number: "))
+    mynumber = random.randint(low, high)
+    print(mynumber)
     count = 1
     guess_number = int(input("Your guess? "))
-    while (guess_number != fix_number):
+    guessed = True
+    while (guess_number != mynumber):
        #midpoint = int((low + high) / 2)
-       if (guess_number > fix_number):
+        if (guess_number > mynumber):
             #low = midpoint
             #print(low)
             print("Your guess is too high, try again.")
             count += 1
             guess_number = int(input("Your guess? "))        
-       else:
+        else:
             #high = midpoint
             #print(high)
             print("Your guess is too low, try again.")
             count += 1
             guess_number = int(input("Your guess? "))
-    print(f"Well done, {name}! You found my number in {count} tries!")
-    scores.append(count)
+        if count > maxCount:
+            print("Too many tries")
+            guessed = False
+            break
+    if guessed:
+        print(f"Well done, {name}! You found my number in {count} tries!")
+        scores.append(count)
     moreRounds = input("Do you want to continue play? Y for yes and N for no ")
     return moreRounds
+
     
 while moreRounds.lower() == "y":
     moreRounds = guessing()
 if moreRounds.lower() == "n":
-    print(scores)
     bestScore = min(scores)
     print(f"Congrats! Your best score is {bestScore} tries!")
